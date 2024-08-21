83af4bf2b22fa583e0bb2814c6d140c969075912
Gabriel28 <nasc.gabriel.1997@gmail.com>
2024-08-20 23:14:05 -0300

    import authors file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import books from "./routes.js";
+import books from "./bookRoutes.js";
+import authors from "./authorRoutes.js";
 
 const app = express();
 const routes = (app) => {
@@ -9,7 +10,8 @@ const routes = (app) => {
 
     app.use(
         express.json(), 
-        books
+        books,
+        authors
     );
 }
 
