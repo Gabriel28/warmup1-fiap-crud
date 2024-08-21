93a246f46570ba6d74878f0ce038716740d31a44
Gabriel28 <nasc.gabriel.1997@gmail.com>
2024-08-21 01:02:12 -0300

    Change type author to use the objectId of authros collection

--- src/models/Book.js
+++ src/models/Book.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const bookSchema = new mongoose.Schema({
     id: {type: String}, // Generate Auto
     title: {type: String, required: true},
-    author: {type: String, required: true},
-    editor: {type: String, required: true},
+    author: {type: mongoose.Schema.Types.ObjectId, ref: 'authors', required: true},
+    publisher: {type: String, required: true},
     pag: {type: Number}
 });
 
